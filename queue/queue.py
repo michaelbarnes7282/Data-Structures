c97ed690985ae7630d6ef8728fd4752d8df897ef
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-07-13 13:36:47 -0700

    finished MVP

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,49 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+from linked_list import Node
+from linked_list import LinkedList
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
+        return value
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            removed = self.storage.head.value
+            self.storage.remove_head()
+            self.size -= 1
+            return removed
+        else:
+            pass
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+#         self.size += 1
+#         return value
+
+#     def dequeue(self):
+#         if self.size > 0:
+#             removed = self.storage[0]
+#             self.storage.pop(0)
+#             self.size -= 1
+#             return removed
+#         else:
+#             pass
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,54 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from linked_list import Node
+from linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
+        return value
 
     def pop(self):
-        pass
+        if self.size > 1:
+            removed = self.storage.tail.value
+            self.storage.remove_tail()
+            self.size -= 1
+            return removed
+        elif self.size == 1:
+            removed = self.storage.head.value
+            self.storage.remove_head()
+            self.size -= 1
+            return removed
+        else:
+            pass
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.storage.append(value)
+#         self.size += 1
+#         return value
+
+#     def pop(self):
+#         if self.size > 0:
+#             removed = self.storage[self.size - 1]
+#             self.storage.pop(self.size - 1)
+#             self.size -= 1
+#             return removed
+#         else:
+#             pass
